8fab8cf21ac448ec8ed30b688d0882aa310bfe5c
Brett Zamir <brettz9@yahoo.com>
2014-02-26 18:11:03 +0800

    Prepare to pass info re: selected node

--- data/context-menu.js
+++ data/context-menu.js
@@ -1,4 +1,17 @@
 /*globals self*/
 self.on('click', function (node, data) {
-	self.postMessage(data); // We need privs on the dialogs we open
+	var msg = {
+		textContent: node.textContent,
+		innerHTML: node.innerHTML,
+		data: data
+	};
+	var nodeName = node.nodeName.toLowerCase();
+	if (nodeName === 'a' && node.hasAttribute('href')) {
+		msg.href = node.getAttribute('href');
+	}
+	else if (nodeName === 'img' && node.hasAttribute('src')) {
+		msg.src = node.getAttribute('src');
+	}
+	
+	self.postMessage(msg); // We need privs on the dialogs we open
 });
