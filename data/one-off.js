1b91c9e4d076c9f4543d599811ba90bdf16f7bf2
Brett Zamir <brettz9@yahoo.com>
2014-06-10 08:45:42 +0800

    More polyfilling and prepare to serialize form

--- data/one-off.js
+++ data/one-off.js
@@ -325,19 +325,25 @@ jml('div', [
 			]],
 			['button', {$on: {click: function () {
 				var dialog = jml('dialog', {open: ''}, [
+					
+					
+					
 					['button', {style: 'margin-top: 40px', $on: {click: function () {
-						dialog.close();
+						var formValues = {}; // Todo: base on form data
+						dialog.close(JSON.stringify(formValues));
 					}}}, [_("Save")]]
 				], document.body);
-				// Until http://www.w3.org/html/wg/drafts/html/master/interactive-elements.html#the-dialog-element is supported
-				dialog.close = function (returnValue) {
-					this.removeAttribute('open');
-					dialog.returnValue = returnValue;
-					// this.parentNode.removeChild(this);
-				};
 				// Alter styling somewhat from W3C-recommended defaults (see http://www.w3.org/html/wg/drafts/html/master/rendering.html#flow-content-0 )
 				dialog.style.height = '500px';
 				dialog.style.backgroundColor = 'lightgray';
+
+				// Rough polyfilling until this is supported: http://www.w3.org/html/wg/drafts/html/master/interactive-elements.html#the-dialog-element
+				dialog.returnValue = '';
+				dialog.close = function (returnValue) {
+					this.removeAttribute('open');
+					dialog.returnValue = String(returnValue);
+					// this.parentNode.removeChild(this);
+				};
 			}}}, [_("Restrict contexts")]]
 		]],
 		['table', [
